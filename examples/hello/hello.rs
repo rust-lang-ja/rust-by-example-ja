7a0e134c9d699e357dc7023aec7c6630c17ede8a
joemphilips <joemphilips@gmail.com>
2016-04-28 23:48:29 +0900

    Translate hello.rs

--- examples/hello/hello.rs
+++ examples/hello/hello.rs
@@ -1,14 +1,14 @@
-// This is a comment, and will be ignored by the compiler
-// You can test this code by clicking the "Run" button over there ->
-// or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+// これはコメントです。コンパイラによって無視されます。
+// 右にある「Run」ボタンからこのコードをテストできます。
+// キーボードを使いたければ「Ctrl + Enter」もOKです。
 
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
+// このコードは編集可能です。ぜひハックしてみましょう！
+// 「Reset」ボタンでいつでも元のコードに戻すことができます ->
 
-// This is the main function
+// main関数です
 fn main() {
-    // The statements here will be executed when the compiled binary is called
+    // コンパイルされたバイナリが実行されるとこの関数が呼び出されます
 
-    // Print text to the console
+    // コンソールに文字列を出力する
     println!("Hello World!");
 }
