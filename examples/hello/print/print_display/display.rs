042d5aea26dee6220fd07a36c39a013eb053419f
joemphilips <joemphilips@gmail.com>
2016-04-29 16:52:19 +0900

    translate print_display

--- examples/hello/print/print_display/display.rs
+++ examples/hello/print/print_display/display.rs
@@ -1,11 +1,11 @@
 use std::fmt; // Import `fmt`
 
-// A structure holding two numbers. `Debug` will be derived so the results can
-// be contrasted with `Display`.
+// 2つの数字を扱うための構造体です。出力を`Display`と比較するため`Debug`
+// をDeriveしています
 #[derive(Debug)]
 struct MinMax(i64, i64);
 
-// Implement `Display` for `MinMax`.
+// `MinMax`用の`Display`を実装しています。
 impl fmt::Display for MinMax {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         // Use `self.number` to refer to each positional data point.
@@ -13,17 +13,17 @@ impl fmt::Display for MinMax {
     }
 }
 
-// Define a structure where the fields are nameable for comparison.
+// 比較のため、フィールドに名前をつけれる様な構造体を定義しましょう
 #[derive(Debug)]
 struct Point2 {
     x: f64,
     y: f64,
 }
 
-// Similarly, implement for Point2
+// 先程と同様にして、Point2用の`Display`を実装しています。
 impl fmt::Display for Point2 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Customize so only `x` and `y` are denoted.
+        // `x`と`y`のみが明示的になるようにカスタマイズ
         write!(f, "x: {}, y: {}", self.x, self.y)
     }
 }
@@ -48,7 +48,7 @@ fn main() {
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
 
-    // Error. Both `Debug` and `Display` were implemented but `{:b}`
-    // requires `fmt::Binary` to be implemented. This will not work.
+    // `Debug`と`Display`は実装されていますが、`fmt::Binary`はされていないため
+    // `{:b}`使用している以下の例はエラーになります、
     // println!("What does Point2D look like in binary: {:b}?", point);
 }
