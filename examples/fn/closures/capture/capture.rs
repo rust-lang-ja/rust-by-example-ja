473001acdd12092a3125300813846d9e563ac1dc
joemphilips <joemphilips@gmail.com>
2016-05-22 15:31:48 +0900

    translate capture

--- examples/fn/closures/capture/capture.rs
+++ examples/fn/closures/capture/capture.rs
@@ -1,54 +1,56 @@
 fn main() {
     use std::mem;
-    
+
     let color = "green";
 
-    // A closure to print `color` which immediately borrows (`&`)
-    // `color` and stores the borrow and closure in the `print`
-    // variable. It will remain borrowed until `print` goes out of
-    // scope. `println!` only requires `by reference` so it doesn't
-    // impose anything more restrictive.
+    // `color`をプリントするためのクロージャ。
+    // これは`color`を借用(`&`)し、その借用とクロージャを`print`
+    // という名の変数に保持する。
+    // 借用は`print`がスコープから出るまで続く。
+    // `println!`は参照を与えれば機能するので、これ以上なにかする必要はない。
     let print = || println!("`color`: {}", color);
 
-    // Call the closure using the borrow.
+    // 借用を行ったクロージャをコールする。
     print();
     print();
 
     let mut count = 0;
 
-    // A closure to increment `count` could take either `&mut count`
-    // or `count` but `&mut count` is less restrictive so it takes
-    // that. Immediately borrows `count`.
+    // `count`をインクリメントするためのクロージャ。`count`と`&mut count`
+    // の両方を取ることができるが、後者のほうが制限が少ないため、
+    // (訳注: `count`だと`&mut count`と違い、一度しか呼ぶことができない。)
+    // そちらを取る。直後に`count`を借用する。
     //
-    // A `mut` is required on `inc` because a `&mut` is stored inside.
-    // Thus, calling the closure mutates the closure which requires
-    // a `mut`.
+    // `int`には`mut`をつける必要がある。なぜならミュータブルな型が
+    // 中で使用されているからである。ミュータブルなクロージャは呼ぶたびに
+    // 内部変数を変更する。
     let mut inc = || {
         count += 1;
         println!("`count`: {}", count);
     };
 
-    // Call the closure.
+    // クロージャを実行
     inc();
     inc();
 
     //let reborrow = &mut count;
-    // ^ TODO: try uncommenting this line.
-    
-    // A non-copy type.
+    // ^ TODO: この行のコメントアウトを解除しましょう。
+
+    // 代入によりコピーではなくムーブが起きる型
+    // (訳注: ヒープ上の値へのポインタであるため、コピーすると
+    // 所有者が2つできてしまう。)
     let movable = Box::new(3);
 
-    // `mem::drop` requires `T` so this must take by value. A copy type
-    // would copy into the closure leaving the original untouched.
-    // A non-copy must move and so `movable` immediately moves into
-    // the closure.
+    // `mem::drop`は`T`(ジェネリック型)を取る必要があるため、このクロージャは
+    // 参照ではなく値を取る。その場合、もしもコピー可能な値ならば、
+    // 元の値はそのままでコピーのみを取る。不可能ならば値そのものを移動させる。
     let consume = || {
         println!("`movable`: {:?}", movable);
         mem::drop(movable);
     };
 
-    // `consume` consumes the variable so this can only be called once.
+    // `consume`は変数を消費(開放)するため、一度しか呼び出すことができない。
     consume();
     //consume();
-    // ^ TODO: Try uncommenting this line.
+    // ^ TODO: この行のコメントアウトを解除しましょう。
 }
