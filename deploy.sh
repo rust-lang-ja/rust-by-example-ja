bf36a597f927d58fbeec40a7471437f22c989aec
joemphilips <joemphilips@gmail.com>
2016-05-29 20:08:13 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ rev=$(git rev-parse --short HEAD)
 cd stage/_book
 
 git init
-git remote add upstream "https://github.com/rust-lang-ja/rust-by-example-ja.git"
+git config user.name "joemphilips"
+git config user.email "joemphilips@gmail.com"
+git remote add upstream "https://$GH_TOKEN@github.com/rust-lang-ja/rust-by-example-ja.git"
 git fetch upstream && git reset upstream/gh-pages
 
 # echo "rustbyexample.com" > CNAME
