b4af062ded4e109a24499d9ca6035252539fb2aa
joemphilips <joemphilips@gmail.com>
2016-06-05 00:24:46 +0900

    translate lifetime

--- examples/scope/lifetime/lifetime.rs
+++ examples/scope/lifetime/lifetime.rs
@@ -1,22 +1,20 @@
-// Lifetimes are annotated below with lines denoting the creation
-// and destruction of each variable.
-// `i` has the longest lifetime because its scope entirely encloses 
-// both `borrow1` and `borrow2`. The duration of `borrow1` compared 
-// to `borrow2` is irrelevant since they are disjoint.
+// 以下では、変数の作成から破棄までのライフタイムを線で示しています。
+// `i`は最長のライフタイムを持ち、そのスコープは`borrow1`および`borrow2`
+// のスコープを完全に包含します。`borrow1`と`borrow2`の存続期間は一切重なりません。
 fn main() {
-    let i = 3; // Lifetime for `i` starts. ────────────────┐
-    //                                                     │
-    { //                                                   │
-        let borrow1 = &i; // `borrow1` lifetime starts. ──┐│
-        //                                                ││
-        println!("borrow1: {}", borrow1); //              ││
-    } // `borrow1 ends. ──────────────────────────────────┘│
-    //                                                     │
-    //                                                     │
-    { //                                                   │
-        let borrow2 = &i; // `borrow2` lifetime starts. ──┐│
-        //                                                ││
-        println!("borrow2: {}", borrow2); //              ││
-    } // `borrow2` ends. ─────────────────────────────────┘│
-    //                                                     │
-}   // Lifetime ends. ─────────────────────────────────────┘
+    let i = 3; // `i`のライフタイムが開始      ────────────────┐
+    //                                                         │
+    { //                                                       │
+        let borrow1 = &i; // `borrow1`のライフタイム開始    ──┐│
+        //                                                    ││
+        println!("borrow1: {}", borrow1); //                  ││
+    } // `borrow1`が終了    ──────────────────────────────────┘│
+    //                                                         │
+    //                                                         │
+    { //                                                       │
+        let borrow2 = &i; // `borrow2`のライフタイム開始    ──┐│
+        //                                                    ││
+        println!("borrow2: {}", borrow2); //                  ││
+    } // `borrow2`が終了     ─────────────────────────────────┘│
+    //                                                         │
+}   // ライフタイムの終了 ─────────────────────────────────────┘
