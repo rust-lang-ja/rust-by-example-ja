7a993b306399834de84cc3c4572156c7892bd8ff
joemphilips <joemphilips@gmail.com>
2016-06-15 01:26:02 +0900

    translate assoc_items/the_problem

--- examples/generics/assoc_items/the_problem/problem.rs
+++ examples/generics/assoc_items/the_problem/problem.rs
@@ -1,28 +1,29 @@
 struct Container(i32, i32);
 
-// A trait which checks if 2 items are stored inside of container.
-// Also retrieves first or last value.
+// ２つの要素がコンテナ型の中にあることをチェックするトレイト
+// また、最初と最後の値を取得することもできる
 trait Contains<A, B> {
-    fn contains(&self, &A, &B) -> bool; // Explicitly requires `A` and `B`.
-    fn first(&self) -> i32; // Doesn't explicitly require `A` or `B`.
-    fn last(&self) -> i32;  // Doesn't explicitly require `A` or `B`.
+    fn contains(&self, &A, &B) -> bool; // `A`と`B`の両方を明示的に要求する
+    fn first(&self) -> i32; // `A`、`B`いずれも要求しない
+    fn last(&self) -> i32;  // `A`、`B`いずれも要求しない
+
 }
 
 impl Contains<i32, i32> for Container {
-    // True if the numbers stored are equal.
+    // コンテナ内の２つの要素が等しいければTrueを返す
     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
         (&self.0 == number_1) && (&self.1 == number_2)
     }
 
-    // Grab the first number.
+    // ひとつ目の値を取得
     fn first(&self) -> i32 { self.0 }
 
-    // Grab the last number.
+    // 最後(2つめ)の値を取得
     fn last(&self) -> i32 { self.1 }
 }
 
-// `C` contains `A` and `B`. In light of that, having to express `A` and
-// `B` again is a nuisance.
+// `A`と`B`は`C`に保持されていることを考慮すると、`A`と`B`を
+// ２度も書くのは面倒
 fn difference<A, B, C>(container: &C) -> i32 where
     C: Contains<A, B> {
     container.last() - container.first()
