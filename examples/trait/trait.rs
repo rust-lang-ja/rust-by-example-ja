5b49f17bd09c44451c29960f33849acc770d53d1
joemphilips <joemphilips@gmail.com>
2016-06-30 00:08:02 +0900

    translat traits

--- examples/trait/trait.rs
+++ examples/trait/trait.rs
@@ -1,14 +1,16 @@
 struct Sheep { naked: bool, name: &'static str }
 
 trait Animal {
-    // Static method signature; `Self` refers to the implementor type.
+    // スタティックメソッドのシグネチャ。
+    // `Self` はこのトレイトを実装している型になる。
     fn new(name: &'static str) -> Self;
 
-    // Instance method signatures; these will return a string.
+    // インスタンスメソッドのシグネチャ。
+    // これらの関数は文字列を返す。
     fn name(&self) -> &'static str;
     fn noise(&self) -> &'static str;
 
-    // Traits can provide default method definitions.
+    // メソッドのデフォルトの挙動を定義することもできる。
     fn talk(&self) {
         println!("{} says {}", self.name(), self.noise());
     }
@@ -21,7 +23,8 @@ impl Sheep {
 
     fn shear(&mut self) {
         if self.is_naked() {
-            // Implementor methods can use the implementor's trait methods.
+            // メソッドをある型に実装する際に、その型のトレイトメソッドを
+            // 使用することができる。
             println!("{} is already naked...", self.name());
         } else {
             println!("{} gets a haircut!", self.name);
@@ -31,9 +34,9 @@ impl Sheep {
     }
 }
 
-// Implement the `Animal` trait for `Sheep`.
+// `Animal`というトレイトを`Sheep`に実装する。
 impl Animal for Sheep {
-    // `Self` is the implementor type: `Sheep`.
+    // `Self`は実装対象の型: ここでは`Sheep`
     fn new(name: &'static str) -> Sheep {
         Sheep { name: name, naked: false }
     }
@@ -49,18 +52,18 @@ impl Animal for Sheep {
             "baaaaah!"
         }
     }
-    
-    // Default trait methods can be overridden.
+
+    // デフォルトのトレイトメソッドはオーバーライドすることができる。
     fn talk(&self) {
-        // For example, we can add some quiet contemplation.
+        // 例えば、静かに熟考させてみる。
         println!("{} pauses briefly... {}", self.name, self.noise());
     }
 }
 
 fn main() {
-    // Type annotation is necessary in this case.
+    // この場合、型アノテーションが必須。
     let mut dolly: Sheep = Animal::new("Dolly");
-    // TODO ^ Try removing the type annotations.
+    // TODO ^ ここの型アノテーションを消してみましょう。
 
     dolly.talk();
     dolly.shear();
