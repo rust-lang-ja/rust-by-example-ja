e94da4f79bc14c9db7be551647b916adb5e34c54
joemphilips <joemphilips@gmail.com>
2016-07-02 18:57:25 +0900

    translate iter

--- examples/trait/iter/iter.rs
+++ examples/trait/iter/iter.rs
@@ -3,34 +3,35 @@ struct Fibonacci {
     next: u32,
 }
 
-// Implement `Iterator` for `Fibonacci`.
-// The `Iterator` trait only requires a method to be defined for the `next` element.
+// `Iterator`を`Fibonacci`に対して実装する。
+// `Iterator`トレイトが実装を要求するのは`next`というフィールド
+// を決定するメソッドのみ。
 impl Iterator for Fibonacci {
     type Item = u32;
-    
-    // Here, we define the sequence using `.curr` and `.next`.
-    // The return type is `Option<T>`:
-    //     * When the `Iterator` is finished, `None` is returned.
-    //     * Otherwise, the next value is wrapped in `Some` and returned.
+
+    // ここではイテレーションの流れを`.curr`と`.next`を使用して定義している。
+    // 返り値の型は`Option<T>`で、これは:
+    //     * `Iterator`が終了した時は`None`を返し、
+    //     * そうでなければ値をラップした`Some`を返す。
     fn next(&mut self) -> Option<u32> {
         let new_next = self.curr + self.next;
 
         self.curr = self.next;
         self.next = new_next;
 
-        // Since there's no endpoint to a Fibonacci sequence, the `Iterator` 
-        // will never return `None`, and `Some` is always returned.
+        // フィボナッチ数列には終端がないので、`Iterator`は決して
+        // `None`を返さず、常に`Some`が返される。
         Some(self.curr)
     }
 }
 
-// Returns a Fibonacci sequence generator
+// フィボナッチ数列のジェネレータを返す。
 fn fibonacci() -> Fibonacci {
     Fibonacci { curr: 1, next: 1 }
 }
 
 fn main() {
-    // `0..3` is an `Iterator` that generates: 0, 1, and 2.
+    // `0..3`は0, 1, 2をジェネレートする`Iterator`
     let mut sequence = 0..3;
 
     println!("Four consecutive `next` calls on 0..3");
@@ -39,20 +40,20 @@ fn main() {
     println!("> {:?}", sequence.next());
     println!("> {:?}", sequence.next());
 
-    // `for` works through an `Iterator` until it returns `None`.
-    // Each `Some` value is unwrapped and bound to a variable (here, `i`).
+    // `for`は`None`を返すまで、イテレータを舐めていき、出てきた`Some`を
+    // アンラップして変数(ここでは`i`)に束縛する。
     println!("Iterate through 0..3 using `for`");
     for i in 0..3 {
         println!("> {}", i);
     }
 
-    // The `take(n)` method reduces an `Iterator` to its first `n` terms.
+    // `take(n)`メソッドは`Iterator`をはじめから`n`番目の要素までの部分に減らす。
     println!("The first four terms of the Fibonacci sequence are: ");
     for i in fibonacci().take(4) {
         println!("> {}", i);
     }
 
-    // The `skip(n)` method shortens an `Iterator` by dropping its first `n` terms.
+    // `skip(n)`メソッドは`Iterator`のはじめから`n`番目までの要素をとばす。
     println!("The next four terms of the Fibonacci sequence are: ");
     for i in fibonacci().skip(4).take(4) {
         println!("> {}", i);
@@ -60,7 +61,7 @@ fn main() {
 
     let array = [1u32, 3, 3, 7];
 
-    // The `iter` method produces an `Iterator` over an array/slice.
+    // `iter`メソッドは配列やスライスからイテレータを作成する。
     println!("Iterate the following array {:?}", &array);
     for i in array.iter() {
         println!("> {}", i);
