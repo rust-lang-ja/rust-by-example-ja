5cd4c7e51e508c004eb08731a308e5b9ba779a4d
joemphilips <joemphilips@gmail.com>
2016-07-03 01:31:27 +0900

    translate channels

--- examples/std_misc/channels/channels.rs
+++ examples/std_misc/channels/channels.rs
@@ -5,35 +5,36 @@ use std::thread;
 static NTHREADS: i32 = 3;
 
 fn main() {
-    // Channels have two endpoints: the `Sender<T>` and the `Receiver<T>`,
-    // where `T` is the type of the message to be transferred
-    // (type annotation is superfluous)
+    // チャネルには`Sender<T>`と`Receiver<T>`という2つのエンドポイントがある。
+    // ここで、`T`は送信されるメッセージの型である。
+    // (型アノテーションは必須ではない。)
     let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
 
     for id in 0..NTHREADS {
-        // The sender endpoint can be copied
+        // 送信者エンドポイントはコピーすることができる。
         let thread_tx = tx.clone();
 
-        // Each thread will send its id via the channel
+        // ここでは、それぞれのスレッドが自身のIDを送信している。
         thread::spawn(move || {
-            // The thread takes ownership over `thread_tx`
-            // Each thread queues a message in the channel
+            // スレッドは`thread_tx`の所有権をとり、それぞれのスレッドは
+            // メッセージをチャネルにキューイングする。
             thread_tx.send(id).unwrap();
 
-            // Sending is a non-blocking operation, the thread will continue
-            // immediately after sending its message
+            // 送信はノンブロッキングなオペレーションなので、
+            // メッセージを送信した後もすぐに実行を継続する。
             println!("thread {} finished", id);
         });
     }
 
-    // Here, all the messages are collected
+    // ここで、全てのメッセージが収集される。
     let mut ids = Vec::with_capacity(NTHREADS as usize);
     for _ in 0..NTHREADS {
-        // The `recv` method picks a message from the channel
-        // `recv` will block the current thread if there no messages available
+        // `recv`メソッドはチャネルからメッセージを取り出す。
+        // もし取り出せるメッセージが存在しない場合、`recv`は
+        // 現在のスレッドをブロックする。
         ids.push(rx.recv());
     }
 
-    // Show the order in which the messages were sent
+    // メッセージが送信された順番を表示
     println!("{:?}", ids);
 }
