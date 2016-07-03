de21e2487cdb22aebf204dc253125075ca4c4e4a
joemphilips <joemphilips@gmail.com>
2016-07-03 15:23:25 +0900

    translate process

--- examples/std_misc/process/input.md
+++ examples/std_misc/process/input.md
@@ -1,7 +1,9 @@
-The `ProcessOutput` struct represents the output of a finished child process,
-and the `Command` struct is a process builder.
+<!-- The `ProcessOutput` struct represents the output of a finished child process,
+and the `Command` struct is a process builder. -->
+`ProcessOutput`構造体は終了したプロセスのアウトプットを表し、`Command`構造体はプロセスの作成を行います。
 
 {process.play}
 
-(You are encouraged to try the previous example with an incorrect flag passed
-to `rustc`)
+<!-- (You are encouraged to try the previous example with an incorrect flag passed
+to `rustc`) -->
+(余裕があれば、上の例で`rustc`に不正なフラグを渡し、どうなるか見てみましょう)
--- examples/std_misc/process/pipe/pipe.rs
+++ examples/std_misc/process/pipe/pipe.rs
@@ -6,7 +6,7 @@ static PANGRAM: &'static str =
 "the quick brown fox jumped over the lazy dog\n";
 
 fn main() {
-    // Spawn the `wc` command
+    // `wc`コマンドを起動する。
     let process = match Command::new("wc")
                                 .stdin(Stdio::piped())
                                 .stdout(Stdio::piped())
@@ -15,23 +15,22 @@ fn main() {
         Ok(process) => process,
     };
 
-    // Write a string to the `stdin` of `wc`.
+    // `wc`の`stdin`に文字列を書き込む。
     //
-    // `stdin` has type `Option<ChildStdin>`, but since we know this instance
-    // must have one, we can directly `unwrap` it.
+    // `stdin`は`Option<ChildStdin>`型を持つが、今回は値を持っていることが
+    // 確かなので、いきなり`unwrap`してしまってよい。
     match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
         Err(why) => panic!("couldn't write to wc stdin: {}",
                            Error::description(&why)),
         Ok(_) => println!("sent pangram to wc"),
     }
 
-    // Because `stdin` does not live after the above calls, it is `drop`ed,
-    // and the pipe is closed.
-    //
-    // This is very important, otherwise `wc` wouldn't start processing the
-    // input we just sent.
+    // `stdin`は上のプロセスコールのあとには有効でないので、`drop`され、
+    // パイプはcloseされる。
+    // (これは非常に重要です。というのもcloseしないと`wc`は
+    // 送った値の処理を開始しないからです。)
 
-    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
+    // `stdout`フィールドも`Option<ChildStdout>`型なのでアンラップする必要がある
     let mut s = String::new();
     match process.stdout.unwrap().read_to_string(&mut s) {
         Err(why) => panic!("couldn't read wc stdout: {}",
