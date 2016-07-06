d54b04ea0b5c73519208e2d745fbf081488d7ad8
joemphilips <joemphilips@gmail.com>
2016-07-06 22:55:45 +0900

    translate hash

--- examples/std/hash/hash.rs
+++ examples/std/hash/hash.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 fn call(number: &str) -> &str {
     match number {
-        "798-1364" => "We're sorry, the call cannot be completed as dialed. 
+        "798-1364" => "We're sorry, the call cannot be completed as dialed.
             Please hang up and try again.",
         "645-7689" => "Hello, this is Mr. Awesome's Pizza. My name is Fred.
             What can I get for you today?",
@@ -10,7 +10,7 @@ fn call(number: &str) -> &str {
     }
 }
 
-fn main() { 
+fn main() {
     let mut contacts = HashMap::new();
 
     contacts.insert("Daniel", "798-1364");
@@ -18,14 +18,16 @@ fn main() {
     contacts.insert("Katie", "435-8291");
     contacts.insert("Robert", "956-1745");
 
-    // Takes a reference and returns Option<&V>
+    // 参照をとり、Option<&V>を返す。
     match contacts.get(&"Daniel") {
         Some(&number) => println!("Calling Daniel: {}", call(number)),
         _ => println!("Don't have Daniel's number."),
     }
 
-    // `HashMap::insert()` returns `None`
-    // if the inserted value is new, `Some(value)` otherwise
+    // `HashMap::insert()`は
+    // insertされた値が新しい場合は`None`を
+    // そうでなければ`Some(value)`を返す。
+
     contacts.insert("Daniel", "164-6743");
 
     match contacts.get(&"Ashley") {
@@ -33,11 +35,11 @@ fn main() {
         _ => println!("Don't have Ashley's number."),
     }
 
-    contacts.remove(&("Ashley")); 
+    contacts.remove(&("Ashley"));
 
-    // `HashMap::iter()` returns an iterator that yields 
-    // (&'a key, &'a value) pairs in arbitrary order.
+    // `HashMap::iter()`は(&'a key, &'a value)
+    // のペアを順不同で産出するイテレータを返す
     for (contact, &number) in contacts.iter() {
-        println!("Calling {}: {}", contact, call(number)); 
+        println!("Calling {}: {}", contact, call(number));
     }
 }
