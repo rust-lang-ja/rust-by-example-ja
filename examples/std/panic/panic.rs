927ef246153df197a42149553c6937e938bde50d
joemphilips <joemphilips@gmail.com>
2016-07-06 22:35:01 +0900

    translate panic

--- examples/std/panic/panic.rs
+++ examples/std/panic/panic.rs
@@ -1,22 +1,23 @@
-// Re-implementation of integer division (/)
+// 整数の除法(/)の再実装
 fn division(dividend: i32, divisor: i32) -> i32 {
     if divisor == 0 {
-        // Division by zero triggers a panic
+        // ゼロによる除算はパニックを引き起こす
         panic!("division by zero");
     } else {
         dividend / divisor
     }
 }
 
-// The `main` task
+// `main`のタスク
 fn main() {
-    // Heap allocated integer
+    // ヒープ上の整数
     let _x = Box::new(0i32);
 
     // This operation will trigger a task failure
+    // このオペレーションはタスクの失敗を引き起こす
     division(3, 0);
 
     println!("This point won't be reached!");
 
-    // `_x` should get destroyed at this point
+    // `_x`はここに到達する前に破棄される。
 }
