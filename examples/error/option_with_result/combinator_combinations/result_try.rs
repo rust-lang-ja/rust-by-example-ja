21c7ee7115bce3e8d1d9063126f172e89acdf702
joemphilips <joemphilips@gmail.com>
2016-07-20 13:31:15 +0900

    translate option_with_result

--- examples/error/option_with_result/combinator_combinations/result_try.rs
+++ examples/error/option_with_result/combinator_combinations/result_try.rs
@@ -3,8 +3,8 @@ use std::fs::File;
 
 type Result<T> = std::result::Result<T, String>;
 
-// Setup to make this work. Create two files with some info. Ignore the
-// return values because we don't care about them here.
+// 以下が動作するための下準備。ファイルを作成して書き込んでおく。
+// 返り値はここではどうでもよいので無視する。
 fn setup() {
     File::create("a")
         .and_then(|mut file| file.write_all(b"grape"))
@@ -15,28 +15,28 @@ fn setup() {
         .unwrap();
 }
 
-// Get the data from each file with the data stored in a `Result`.
+// ファイルからデータを取り出し、値を`Result`として返す。
 fn get_data(path: &str) -> Result<String> {
     File::open(path)
         .map_err(|err| err.to_string())
         .and_then(|mut file| {
             let mut contents = String::new();
 
-            // Read the data into `contents`.
+            // データを`contents`に読み込む。
             file.read_to_string(&mut contents)
                 .map_err(|err| err.to_string())
-                // Ignore the output `read_to_string` returns and return `contents`.
+                // `read_to_string`の返り値は無視し、`contents`を返す。
                 .map(|_| contents)
         })
 }
 
-// Concat the contents of the two files together into a new `Result`.
+// 二つのファイルの中身を結合し、新しい`Result`にして返す。
 fn concat(filename_a: &str, filename_b: &str) -> Result<String> {
     let (data_a, data_b) = (get_data(filename_a), get_data(filename_b));
-    
+
     data_a.and_then(|a|
-        // Return `Ok` when both `a` and `b` are `Ok`. Otherwise return
-        // whichever has the first `Err`.
+        // `a`と`b`が両方とも`Ok`ならば`Ok`を返し、そうでなければ
+        // 先に`Err`を挙げた方の`Err`を返す。
         data_b.and_then(|b| Ok(a + &b))
     )
 }
