12b480c79d9451ce8c6fcd21d6886127453365db
joemphilips <joemphilips@gmail.com>
2016-07-21 00:25:55 +0900

    translate option_with_result/enter_try

--- examples/error/option_with_result/enter_try/try.rs
+++ examples/error/option_with_result/enter_try/try.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 
 type Result<T> = std::result::Result<T, String>;
 
-// Setup to make this work. Create two files with some info.
+// 下準備。ファイルを作成し文字列を書き込む
 fn setup() -> std::io::Result<()> {
     let mut a = try!(File::create("a"));
     try!(a.write_all(b"grape"));
@@ -12,16 +12,16 @@ fn setup() -> std::io::Result<()> {
     b.write_all(b"fruit")
 }
 
-// Get the data from each file with the data stored in a `Result`.
+// それぞれのファイルから`Result`でラップされたデータを取得
 fn get_data(path: &str) -> Result<String> {
-    // `try` unwraps the value or returns the error.
+    // `try`は値をアンラップするかエラーを返す
     let mut file = try!(File::open(path)
-        // Errors still must be converted to strings.
+        // エラーを文字列に変換する必要があるのは今までと同様
         .map_err(|err| err.to_string())
     );
     let mut contents = String::new();
 
-    // Read the data into `contents`.
+    // データを`contents`に読み込む
     try!(file.read_to_string(&mut contents)
         .map_err(|err| err.to_string())
     );
@@ -29,7 +29,7 @@ fn get_data(path: &str) -> Result<String> {
     Ok(contents)
 }
 
-// Concat the contents of the two files together into a new `Result`.
+// ２つのファイルの内容を結合させて新しい`Result`を作成。
 fn concat(a: &str, b: &str) -> Result<String> {
     let (data_a, data_b) = (try!(get_data(a)), try!(get_data(b)));
 
@@ -37,7 +37,7 @@ fn concat(a: &str, b: &str) -> Result<String> {
 }
 
 fn main() {
-    // Ignore this result.
+    // ここの返り値のResultは無視する
     setup().unwrap();
 
     match concat("a", "b") {
