8802358476bb8781a8c0b3eba84da6a5100d7dc9
joemphilips <joemphilips@gmail.com>
2016-07-22 19:28:58 +0900

    translate limits_of_strings

--- examples/error/limits_of_strings/rethink.rs
+++ examples/error/limits_of_strings/rethink.rs
@@ -4,31 +4,31 @@ use std::fmt;
 type Result<T> = std::result::Result<T, DoubleError>;
 
 #[derive(Debug)]
-// Define our error types. These may be customized however is useful for our error
-// handling cases. Now we will be able to defer to the underlying tools error
-// implementation, write our own errors, or something in between.
+// 独自のエラー型を定義する。これならば、ここでのエラーハンドリングの仕方に合わせて
+// 自由にカスタマイズできる。こうすることで、エラーの実装の裏側にあるツール、
+// 独自に定義したエラー、あるいはその両方に従うことが可能となる。
 enum DoubleError {
-    // We don't require any extra info to detail this error.
+    // このエラーに関しては、エラーの詳細を説明するのに追加の情報は必要ない。
     EmptyVec,
-    // We will defer to the parse error implementation for their error. Supplying extra
-    // info would require adding more data to the type.
+    // パースに失敗した場合はParseIntErrorの挙動に追従する。追加の情報を持たせたい
+    // 場合は、この型に、より多くのデータを追加しなくてはならない。
     Parse(ParseIntError),
 }
 
-// How the type is displayed is completely separate from where the errors are generated.
-// We do not need to be concerned that the display style will clutter the complex logic
-// our utility requires. They are separate matters which are handled separately.
+// 型がどのように表示されるかは、そのエラーが生じた場所とは全く関係がない。
+// また、ここで用いている複雑なロジックが、エラーのディスプレイスタイルによって
+// そのままぶちまけられてしまうことも心配する必要はない。それらは別々の問題であって
+// 別々に扱われる。
 //
-// We don't store extra info about the errors. If we had desired, for example, to state
-// which string failed to parse then we can't without modifying our types to carry that
-// information accordingly.
+// ここではエラーに関して、追加の情報を保持してはいない。たとえば、パースに失敗したString
+// がどれであるかを明示したいとする。その場合、独自に定義したエラー型を変更して、その情報を
+// 保持するようにしてやる必要がある。
 impl fmt::Display for DoubleError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             DoubleError::EmptyVec =>
                 write!(f, "please use a vector with at least one element"),
-            // This is a wrapper so defer to the underlying types' own implementation
-            // of `fmt`.
+            // こちらはラッパーなので、内側の型(この場合は`ParseIntError`)の`fmt`の実装に従う。
             DoubleError::Parse(ref e) => e.fmt(f),
         }
     }
@@ -36,10 +36,10 @@ impl fmt::Display for DoubleError {
 
 fn double_first(vec: Vec<&str>) -> Result<i32> {
     vec.first()
-       // Change the error to our new type.
+       // エラーを新しい型のものに変換
        .ok_or(DoubleError::EmptyVec)
        .and_then(|s| s.parse::<i32>()
-            // Update to the new error type here also.
+            // ここでも新しいエラー型へとアップデートさせる。
             .map_err(DoubleError::Parse)
             .map(|i| 2 * i))
 }
